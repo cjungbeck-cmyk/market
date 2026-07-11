097cab2a42e3c34c3edb54cd317cf5fe97080d47
cjungbeck-cmyk <cjungbeck@gmail.com>
2026-07-11 22:00:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
+import os
+import requests
 
+WEBHOOK = os.environ["DISCORD_WEBHOOK"]
+FINNHUB = os.environ["FINNHUB_API_KEY"]
+
+url = "https://finnhub.io/api/v1/quote"
+
+params = {
+    "symbol": "SPY",
+    "token": FINNHUB
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+price = data["c"]
+previous = data["pc"]
+
+change = ((price - previous) / previous) * 100
+
+emoji = "🟢" if change >= 0 else "🔴"
+
+message = f"""
+📈 **Market Test**
+
+{emoji} S&P 500
+Pris: {price:.2f}
+Förändring: {change:.2f}%
+"""
+
+requests.post(
+    WEBHOOK,
+    json={"content": message}
+)
