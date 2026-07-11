1dd2f861b29ca575a67fdbdb6f8527bcff80860c
cjungbeck-cmyk <cjungbeck@gmail.com>
2026-07-11 22:24:10 +0200

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 import os
 import requests
 
-from market import get_market_report
-
 WEBHOOK = os.environ["DISCORD_WEBHOOK"]
-
-message = f"""📈 Market Close
-
-{get_market_report()}
-"""
 
 response = requests.post(
     WEBHOOK,
-    json={"content": message}
+    json={"content": "🚨 TEST FRÅN APP.PY"}
 )
 
-print("Discord status:", response.status_code)
-print("Discord response:", response.text)
+print(response.status_code)
+print(response.text)
