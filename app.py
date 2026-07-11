b5c3fd76ecba06bd9b1f94a9de1d6d01243344a4
cjungbeck-cmyk <cjungbeck@gmail.com>
2026-07-11 22:11:12 +0200

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
 import os
 import requests
 
+from market import get_market_report
+
 WEBHOOK = os.environ["DISCORD_WEBHOOK"]
-FINNHUB = os.environ["FINNHUB_API_KEY"]
-
-url = "https://finnhub.io/api/v1/quote"
-
-params = {
-    "symbol": "SPY",
-    "token": FINNHUB
-}
-
-response = requests.get(url, params=params)
-data = response.json()
-
-price = data["c"]
-previous = data["pc"]
-
-change = ((price - previous) / previous) * 100
-
-emoji = "🟢" if change >= 0 else "🔴"
 
 message = f"""
-📈 **Market Test**
+# 📈 Market Close
 
-{emoji} S&P 500
-Pris: {price:.2f}
-Förändring: {change:.2f}%
+{get_market_report()}
 """
 
 requests.post(
